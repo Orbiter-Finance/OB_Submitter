cb015b32ac16a1a08b227c3c712bae9d384a51c3
YanOctavian <iu4ryhiuenb@proton.me>
2023-08-16 17:55:02 +0800

    update

--- bin/submitter/client/src/rpc.rs
+++ bin/submitter/client/src/rpc.rs
@@ -7,6 +7,9 @@ use state::Error as StateError;
 use state::{address_convert_to_h256, Blake2bHasher, StataTrait, State, H256};
 use std::ops::Deref;
 use std::sync::{Arc, RwLock};
+pub const STATE_ERROR_CODE: i32 = 666;
+pub const RWLOCK_READ_ERROR_CODE: i32 = 888;
+pub const ACCOUNT_NOT_EXISTS_CODE: i32 = 777;
 
 pub struct JsonRpcError(pub ErrorObjectOwned);
 
@@ -20,17 +23,21 @@ impl From<StateError> for JsonRpcError {
     fn from(err: StateError) -> Self {
         JsonRpcError(match err {
             StateError::BincodeError(e) => {
-                ErrorObject::owned(666, format!("error: {:#?}", e), None::<bool>)
+                ErrorObject::owned(STATE_ERROR_CODE, format!("error: {:#?}", e), None::<bool>)
             }
 
             StateError::RocksDBError(e) => {
-                ErrorObject::owned(666, format!("error: {:#?}", e), None::<bool>)
+                ErrorObject::owned(STATE_ERROR_CODE, format!("error: {:#?}", e), None::<bool>)
             }
 
             StateError::SparseMerkleTreeError(e) => {
-                ErrorObject::owned(666, format!("error: {:#?}", e), None::<bool>)
+                ErrorObject::owned(STATE_ERROR_CODE, format!("error: {:#?}", e), None::<bool>)
             }
-            _ => ErrorObject::owned(666, format!("error: unknown err"), None::<bool>),
+            _ => ErrorObject::owned(
+                STATE_ERROR_CODE,
+                format!("error: unknown err"),
+                None::<bool>,
+            ),
         })
     }
 }
@@ -43,13 +50,17 @@ pub struct SubmitterApiServerImpl<'a> {
 impl SubmitterApiServer for SubmitterApiServerImpl<'static> {
     async fn get_balance(&self, address: Address) -> RpcResult<U256> {
         let state = self.state.read().map_err(|_| {
-            ErrorObject::owned(888, format!("error: state read error."), None::<bool>)
+            ErrorObject::owned(
+                RWLOCK_READ_ERROR_CODE,
+                format!("error: state read error."),
+                None::<bool>,
+            )
         })?;
         let balance = state
             .try_get(address_convert_to_h256(address))
             .map_err(|e| Into::<JsonRpcError>::into(e))?
             .ok_or(ErrorObject::owned(
-                777,
+                ACCOUNT_NOT_EXISTS_CODE,
                 format!("error: account is not in off-chain-state."),
                 None::<bool>,
             ))?
@@ -59,7 +70,11 @@ impl SubmitterApiServer for SubmitterApiServerImpl<'static> {
 
     async fn get_root(&self) -> RpcResult<[u8; 32]> {
         let state = self.state.read().map_err(|_| {
-            ErrorObject::owned(888, format!("error: state read error."), None::<bool>)
+            ErrorObject::owned(
+                RWLOCK_READ_ERROR_CODE,
+                format!("error: state read error."),
+                None::<bool>,
+            )
         })?;
         let root = state
             .try_get_root()
@@ -69,7 +84,11 @@ impl SubmitterApiServer for SubmitterApiServerImpl<'static> {
 
     async fn get_proof(&self, address: Address) -> RpcResult<Vec<u8>> {
         let state = self.state.read().map_err(|_| {
-            ErrorObject::owned(888, format!("error: state read error."), None::<bool>)
+            ErrorObject::owned(
+                RWLOCK_READ_ERROR_CODE,
+                format!("error: state read error."),
+                None::<bool>,
+            )
         })?;
         let proof = state
             .try_get_merkle_proof(vec![address_convert_to_h256(address)])
@@ -79,7 +98,11 @@ impl SubmitterApiServer for SubmitterApiServerImpl<'static> {
 
     async fn verify(&self, address: Address, proof: Vec<u8>) -> RpcResult<bool> {
         let state = self.state.read().map_err(|_| {
-            ErrorObject::owned(888, format!("error: state read error."), None::<bool>)
+            ErrorObject::owned(
+                RWLOCK_READ_ERROR_CODE,
+                format!("error: state read error."),
+                None::<bool>,
+            )
         })?;
         let verify = state
             .try_get_merkle_proof(vec![address_convert_to_h256(address)])
